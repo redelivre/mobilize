c180e2a737b1627c667f16516a9417f20ecc374b
thalita <thalita@ethymos.com.br>
2014-10-24 16:39:08 -0200

    social buttons - pinterest, instagram,flickr

--- custom-css.php
+++ custom-css.php
@@ -12,8 +12,8 @@
 	.main-menu.bg-cor .sub-menu { background: <?php echo get_theme_mod('cor-1'); ?>; }
 	
 	body { background-image: url('<?php echo get_theme_mod('background-imagem'); ?>'); background-color: <?php echo get_theme_mod('background-cor'); ?> }
-	#facebook, #twitter, #google-plus, #youtube, #rss { background-color:  <?php echo get_theme_mod('cor-1'); ?>; }
-	#facebook:hover, #twitter:hover, #google-plus:hover, #youtube:hover, #rss:hover { background-color: <?php echo get_theme_mod('cor-2'); ?>; }
+	#facebook, #twitter, #google-plus, #youtube, #rss, #instagram, #pinterest, #flickr { background-color:  <?php echo get_theme_mod('cor-1'); ?>; }
+	#facebook:hover, #twitter:hover, #google-plus:hover, #youtube:hover, #rss:hover, #instagram:hover, #pinterest:hover, #flickr:hover { background-color: <?php echo get_theme_mod('cor-2'); ?>; }
 
 	@media only screen and (max-width: 600px) {
 		ul.menu li a:active {
